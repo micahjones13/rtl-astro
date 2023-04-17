50b1f714a739bd1d3555d8e22276794e7bb0bcfe
Micah Jones <micah.jones@rocketcom.com>
2023-04-17 10:06:09 -0600

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/prefer-screen-queries */
+//input.test.tsx
+import { render, fireEvent, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { within } from "testing-library__dom";
+import RuxInputTest from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//* Make sure to run `npm run astro-test`
+
+describe("RuxInput", () => {
+  test("Input changes with fireEvent", async () => {
+    const { getByTestId, findByDisplayValue } = render(<RuxInputTest />);
+    const ruxInput = getByTestId("rux-input-test");
+    fireEvent.change(ruxInput, { target: { value: "Input!" } });
+    await findByDisplayValue("Input!");
+  });
+
+  test("Input changes with userEvent", async () => {
+    const { getByTestId, findByDisplayValue } = render(<RuxInputTest />);
+    const ruxInput = getByTestId("rux-input-test");
+    let shadowInput: HTMLRuxInputElement = await within(
+      ruxInput
+    ).findByLabelText("Rux Input");
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      userEvent.type(shadowInput, "User input!");
+    });
+    await findByDisplayValue("User input!");
+    expect(shadowInput.value).toBe("User input!");
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// MyForm.tsx
+import { useState } from "react";
+import { RuxInput } from "@astrouxds/react";
 
-function App() {
+function RuxInputTest() {
+  const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RuxInput
+        label="Rux Input"
+        type="text"
+        data-testid="rux-input-test"
+        value={input}
+        onRuxinput={(e: CustomEvent<HTMLRuxInputElement>) => {
+          const target = e.target as HTMLInputElement;
+          setInput(target.value);
+        }}
+      />
     </div>
   );
 }
 
-export default App;
+export default RuxInputTest;
